9e8d1345be9959b78ba0fecfe59110a3783d482e
Manikant Jha <being.manikantjha@gmail.com>
2024-06-24 12:11:48 +0530

    completed user-defined modules

---
+++ calc.js
@@ -0,0 +1,20 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+const subtract = (a, b) => {
+  return a - b;
+};
+
+const multiply = (a, b) => {
+  return a * b;
+};
+
+const divide = (a, b) => {
+  return a / b;
+};
+
+const value = 50;
+
+// module.exports = value;
+module.exports = { add, subtract, multiply, divide, value };
---
+++ data.js
@@ -0,0 +1,7 @@
+const student = {
+  id: 1,
+  name: "Leanne Graham",
+  email: "Sincere@april.biz",
+};
+
+module.exports = student;
