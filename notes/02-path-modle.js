c00f108ac80b3b0dc3668d64b33b1a8c5294210a
Manikant Jha <being.manikantjha@gmail.com>
2024-06-25 12:33:13 +0530

    added comments

--- notes/02-path-modle.js
+++ notes/02-path-modle.js
@@ -1,32 +1,41 @@
 const path = require("path");
 
-console.log("__dirname", __dirname);
-console.log("__filename", __filename);
+// console.log("__dirname", __dirname);
+// console.log("__filename", __filename);
 
+// basename
 // console.log("__dirname", path.basename(__dirname));
 // console.log("__filename", path.basename(__filename));
 
+// delimiter
 // console.log("path.delimiter", path.delimiter);
 
+// isAbsolute
 // console.log("__dirname", path.isAbsolute(__dirname));
 // console.log("__filename", path.isAbsolute("./app.js"));
 
+// parse
 // console.log("__dirname", path.format(path.parse(__filename)));
 // console.log("__dirname", path.format(path.parse(__dirname)));
 
+// join
 // console.log(
 //   "join",
 //   path.join(__dirname, "../folderOne", "folderTwo", "one.jpg")
 // );
 
+// resolve
 // console.log("resolve", path.resolve("/folderOne", "/folderTwo", "one.jpg"));
 
+// extname
 // console.log("extname", path.extname(__filename));
 // console.log("extname", path.extname(__dirname));
 
-// console.log("__filename", path.basename(path.dirname(__filename)));
-// console.log("__dirname", path.basename(path.dirname(__dirname)));
+// dirname
+// console.log("__filename", path.dirname(__filename));
+// console.log("__dirname", path.dirname(__dirname));
 
+// relative
 // console.log(
 //   "relativePath",
 //   path.relative(
@@ -35,4 +44,5 @@ console.log("__filename", __filename);
 //   )
 // );
 
-console.log("win32", path.win32);
+// win32
+// console.log("win32", path.win32);
